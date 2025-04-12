f761d2a66ced2adaa4836ce5c08b757bdfdab283
vkrukhmalev <Krukhmalev02@gmail.com>
2025-04-12 19:10:51 -0400

    Improved clip shader

--- Assets/Scripts/VolumeClip.cs
+++ Assets/Scripts/VolumeClip.cs
@@ -3,28 +3,25 @@ using UnityEngine;
 [ExecuteAlways]
 public class VolumeClip : MonoBehaviour
 {
-    [Tooltip("Material using your Shader Graph with the Custom Function node")]
-    public Material maskMaterial;
-    
-        [Tooltip("List of sphere Transforms")]
-        public List<Transform> sphereTransforms;
-    
-        void Update()
+    [Tooltip("List of sphere Transforms")]
+    public List<Transform> sphereTransforms;
+
+    private void Update()
+    {
+        
+        // clamp to our MAX_SPHERES (64)
+        int count = Mathf.Min(sphereTransforms.Count, 64);
+        var data  = new Vector4[count];
+
+        for (int i = 0; i < count; i++)
         {
-            
-            // clamp to our MAX_SPHERES (64)
-            int count = Mathf.Min(sphereTransforms.Count, 64);
-            var data  = new Vector4[count];
-    
-            for (int i = 0; i < count; i++)
-            {
-                Vector3 p = sphereTransforms[i].position;
-                float   r = sphereTransforms[i].localScale.x / 2;
-                data[i]    = new Vector4(p.x, p.y, p.z, r);
-            }
-    
-            // feed the HLSL globals
-            maskMaterial.SetVectorArray("_Spheres",    data);
-            maskMaterial.SetInt       ("_SphereNumber", count);
+            Vector3 p = sphereTransforms[i].position;
+            float   r = sphereTransforms[i].localScale.x / 2;
+            data[i]    = new Vector4(p.x, p.y, p.z, r);
         }
+
+        // feed the HLSL globals
+        Shader.SetGlobalVectorArray("_Spheres",    data);
+        Shader.SetGlobalInt("_SphereNumber", count);
+    }
 }
