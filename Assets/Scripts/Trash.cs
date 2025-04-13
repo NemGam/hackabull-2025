8b5b68c8fa2972295d60dfb1a06c5f37fd205d28
vkrukhmalev <Krukhmalev02@gmail.com>
2025-04-12 21:24:07 -0400

    Added particle shader and fire

---
+++ Assets/Scripts/Trash.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+public class Trash : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        
+    }
+}
--- Assets/Shaders/SphereMask.hlsl
+++ Assets/Shaders/SphereMask.hlsl
@@ -8,14 +8,17 @@ float4 _Spheres[MAX_SPHERES];   // xyz = center, w = radius
 int   _SphereNumber;
 
 
-float SampleSphereMask_float (float3 worldPos, out float mask)
+float SampleSphereMask_float (float3 worldPos, out float mask, out float minAbsDist)
 {
+    minAbsDist = 1e6;
     // Union of all spheres: bail out as soon as we hit one
     for (int i = 0; i < _SphereNumber; i++)
     {
         float4 sp = _Spheres[i];
         float3  d  = worldPos - sp.xyz;
         // squared‐distance test
+        float  dist  = sqrt(dot(d,d)) - sp.w;
+        minAbsDist = min(minAbsDist, abs(dist));
         if (dot(d, d) < sp.w * sp.w)
         {
             mask =  1.0f;
